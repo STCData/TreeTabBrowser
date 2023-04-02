c83e21247bb6b975b0c2b67efcf0a6b083da859c
stc <standardtemplateconstruct2022@gmail.com>
2023-04-02 10:31:09 +0300

    Initial Commit

---
+++ TreeTabBrowser/TreeTabBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeTabBrowserApp.swift
+//  TreeTabBrowser
+//
+//  Created by standard on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TreeTabBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
